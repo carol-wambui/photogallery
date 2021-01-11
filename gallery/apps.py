38467a4d2ef26b0d7e8e0d30fc46057e4eec0713
carol-wambui <carolwambuidaystar@gmail.com>
2021-01-11 18:27:34 +0300

    create apps

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,5 +1,5 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class GalleryConfig(AppConfig):
-#     name = 'gallery'
+class GalleryConfig(AppConfig):
+    name = 'gallery'

bcf72c4532f318c8390d8af725f39f856458a3eb
carol-wambui <carolwambuidaystar@gmail.com>
2021-01-11 23:34:37 +0300

    deploy to heroku

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]   
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +132,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# django_heroku.settings(locals())
